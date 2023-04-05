40f9758608267497b9b76392b9e4b0d9a9c0a53d
phatvct99 <phatvct99@gmail.com>
2023-04-05 14:48:23 +0700

    add herou

--- database/web_ban_trai_cay.sql
+++ database/web_ban_trai_cay.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th2 01, 2023 lúc 07:19 AM
--- Phiên bản máy phục vụ: 5.7.36
--- Phiên bản PHP: 7.4.26
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Cơ sở dữ liệu: `web_ban_trai_cay`
---
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `login_roles`
---
 
 DROP TABLE IF EXISTS `login_roles`;
 CREATE TABLE IF NOT EXISTS `login_roles` (
@@ -12693,6 +12666,3 @@ INSERT INTO `tbl_xaphuongthitran` (`xaid`, `name_xaphuong`, `type`, `maqh`) VALU
 ('32248', 'Xã Đất Mũi', 'Xã', 973);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
