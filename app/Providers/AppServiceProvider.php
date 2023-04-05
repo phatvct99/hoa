e625b02e677a70454ac12f9fc0ef0b1953911912
phatvct99 <phatvct99@gmail.com>
2023-04-05 15:58:28 +0700

    add 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use App\Models\Brand; 
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Customer;
 use App\Models\Order;
@@ -13,6 +13,9 @@ use App\Models\Products;
 use App\Models\Slider;
 use App\Models\Contact;
 use App\Models\Ads;
+use Illuminate\Support\Facades\Schema;
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -32,24 +35,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-         Paginator::useBootstrap();
-         view()->composer('*',function($view){
+        // if (config('app.env') === 'production') {
+        //     \URL::forceScheme('https');
+        // }
+
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrap();
+        view()->composer('*', function ($view) {
             $product_donut = Products::all()->count();
             $post_donut = Posts::all()->count();
             $order_donut = Order::all()->count();
             $customer_donut = Customer::all()->count();
             $category_donut = Category::all()->count();
             $brand_donut = Brand::all()->count();
-            $slider_home = Slider::where('slider_status','1')->orderBy('slider_id','DESC')->get();
+            $slider_home = Slider::where('slider_status', '1')->orderBy('slider_id', 'DESC')->get();
             $contact_info = Contact::all();
-            $ads_home = Ads::where('ads_status','1')->orderBy('ads_id','DESC')->take(2)->get();
+            $ads_home = Ads::where('ads_status', '1')->orderBy('ads_id', 'DESC')->take(2)->get();
             //count order new
-            $order_new_dashboard = Order::where('order_status',1)->orderBy('order_id','DESC')->get();
+            $order_new_dashboard = Order::where('order_status', 1)->orderBy('order_id', 'DESC')->get();
             $count_order_dashboard = Count($order_new_dashboard);
             //bai viet huu ich
-            $post_huuich = Posts::where('cate_blog_id',9)->get();
-            $view->with(compact('product_donut','post_donut','order_donut','customer_donut','category_donut','brand_donut','slider_home','contact_info','ads_home','order_new_dashboard','count_order_dashboard','post_huuich'));
-         });
-        
+            $post_huuich = Posts::where('cate_blog_id', 9)->get();
+            $view->with(compact('product_donut', 'post_donut', 'order_donut', 'customer_donut', 'category_donut', 'brand_donut', 'slider_home', 'contact_info', 'ads_home', 'order_new_dashboard', 'count_order_dashboard', 'post_huuich'));
+        });
     }
 }
